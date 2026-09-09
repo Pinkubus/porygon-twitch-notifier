bbe6cff3616e77a070690eaaa5a7326e23f6e55c
Tyler Williams <TylerJiggliams@gmail.com>
2026-09-08 22:59:12 -0500

    tmp: test emoji variants

--- _tmp_debug_quotes.py
+++ _tmp_debug_quotes.py
@@ -4,5 +4,6 @@ import discord_roles
 token = os.environ["DISCORD_BOT_TOKEN"]
 channel_id = os.environ["DISCORD_REACTION_CHANNEL_ID"]
 
-ok = discord_roles.add_own_reaction(channel_id, "1547092553814249543", "\u2122\uFE0F", token)
-print(f"Backfilled reaction: {ok}")
+for emoji in ["\u2122\uFE0F", "\u2122"]:
+    ok = discord_roles.add_own_reaction(channel_id, "1547092553814249543", emoji, token)
+    print(f"Tried {emoji!r} ({[hex(ord(c)) for c in emoji]}): {ok}")
